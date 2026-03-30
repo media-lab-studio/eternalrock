2a89dd698713d7625309e256e90930f52ed35cda
MediaLab <114830377+media-lab-studio@users.noreply.github.com>
2026-03-30 18:44:09 +0300

    Update slider.js
    изменил количество слайдов

--- js/slider.js
+++ js/slider.js
@@ -18,7 +18,7 @@ function generateSlides() {
   };
 
   // Количество слайдов (можно легко изменить)
-  const totalSlides = 108;
+  const totalSlides = 130;
 
   // Выбираем случайный индекс для первого слайда (от 0 до totalSlides-1)
   const randomStartIndex = Math.floor(Math.random() * totalSlides);
